9fec16dfe05985cb9164bbd786a4c8fcafabfe3f
Greeninja <nick@greeninja.co.uk>
2015-10-18 19:07:22 +0100

    demo scopes & arrays

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,5 @@
 class Page < ActiveRecord::Base
+	belongs_to :subject
+	has_many :sections
+	
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,12 @@
 class Subject < ActiveRecord::Base
+
+    has_many :pages
+
+	scope :visable, lambda { where(:visable => true) }
+	scope :invisable, lambda { where(:visable => false) }
+	scope :sorted, lambda { order("subjects.position ASC") }
+	scope :newest_first, lambda { order("subjects.created_at DESC") }
+	scope :search, lambda {|query|
+	  where(["name LIKE ?", "%#{query}%"])
+	}
 end
