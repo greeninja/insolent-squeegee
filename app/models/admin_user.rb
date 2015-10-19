aa765e0adf17939b18384fb97b17356c31ef248d
Greeninja <nick@greeninja.co.uk>
2015-10-19 21:00:17 +0100

    associations rails error

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,5 +1,5 @@
 class AdminUser < ActiveRecord::Base
 	# To configure a different table:
 	# self.table_name = "admin_users"
-	has_and_belongs_to_many :editors, :class_name => "AdminUser"
+	has_and_belongs_to_many :pages  
 end
