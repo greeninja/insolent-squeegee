dc5fe53a0f3e61521f15b693d96549f9b0d97d02
Nick Campion <nick.campion@fasthosts.com>
2015-11-14 21:45:56 +0000

    demo controller logging tests

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -26,6 +26,13 @@ class DemoController < ApplicationController
     # render(:text => "test" #syntax error - missing closing bracket
     # render(:text => @something.upcase) # undefined method
     # render(:text => "1" + 1) # add string to integer
+    logger.debug("this is a debug log message")
+    logger.info("This is an Info log message")
+    logger.warn("this is a Warn log message")
+    logger.error("This is an Error log message")
+    logger.fatal("This is a fatal log message")
+    render(:text => 'Logged!')
+  
   end
 
 end
